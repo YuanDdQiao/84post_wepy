c622ef2b730a48ca172f30ae4adbe294c109579f
Singee <imsingee@gmail.com>
2018-06-07 14:30:19 +0800

    api.request auto retry

--- src/utils/api.js
+++ src/utils/api.js
@@ -6,15 +6,16 @@ import 'wepy-async-function'
 const host = 'https://84.singee.site'
 const apiRoot = `${host}/api/v1`
 
+// 分享信息
 const shareInfo = {
   title: '来给我写封信吧！',
   imageUrl: `${host}/media/share/default.png`,
   boxTitle: '看看你的邮件到哪了！'
 }
 
-// 普通请求
+// 普通请求（wx.request 封装）
 const request = async (options, showLoading = true, auth = true) => {
-  // 简化开发，如果传入字符串则转换成 对象
+  // 传入字符串则直接认为是 GET 请求
   if (typeof options === 'string') {
     options = {
       url: options
@@ -24,10 +25,14 @@ const request = async (options, showLoading = true, auth = true) => {
   if (showLoading) {
     wepy.showLoading({ title: '加载中' })
   }
-  // 拼接请求地址
-  options.url = apiRoot + '/' + options.url
 
-  // Authorization
+  // 拼接请求地址
+  let start = /^https?:\/\//
+  if (!start.exec(options.url)) {
+    options.url = apiRoot + '/' + options.url
+  }
+
+  // 权限验证 - Authorization
   if (auth) {
     if (!options.header) {
       options.header = {}
@@ -41,45 +46,58 @@ const request = async (options, showLoading = true, auth = true) => {
   }
 
   // 调用小程序的 request 方法
-  // console.log(options)
   let response
+  let error = false
   try {
     response = await wepy.request(options)
   } catch (e) {
-    if (showLoading) {
-      // 隐藏加载中
-      wepy.hideLoading()
-    }
-
-    wepy.showModal({
-      title: '提示',
-      content: '无法连接服务器，请检查网络后重试',
-      showCancel: false
-    })
-    return null
+    // 无网络或请求超时
+    error = true
   }
-  // console.log(response)
 
   if (showLoading) {
     // 隐藏加载中
     wepy.hideLoading()
   }
 
-  // 服务器异常后给与提示
-  if (response.statusCode === 500) {
-    wepy.showModal({
-      title: '提示',
-      content: '服务器错误，请稍后再试',
-      showCancel: false
-    })
+  let content = '网络错误'
+  let needRelogin = false
+  if (error) {
+    content = '无法连接服务器，请检查网络后重试'
+  } else if (response.statusCode === 200) {
+    return response
+  } else if (response.statusCode === 500) {
+    content = '服务器错误，我们会尽快修复，请稍后再试'
   } else if (response.statusCode === 401) {
-    wepy.showModal({
-      title: '提示',
-      content: '未登录或权限不足',
-      showCancel: false
-    })
+    content = '您当前未登录或权限不足，请重试'
+    needRelogin = true
+  } else if (response.statusCode === 403) {
+    content = '您当前未登录或权限不足，请重试'
+    needRelogin = true
+  } else {
+    content = `未知原因（请求状态码：${response.statusCode}））`
   }
-  return response
+
+  let retryModal = await wepy.showModal({
+    title: '网络请求错误',
+    content: content,
+    confirmText: '重试',
+    showCancel: false
+  })
+
+  if (retryModal.confirm) {
+    console.log('重试网络请求')
+    // 重新登录
+    if (needRelogin) {
+      await login()
+    }
+    // 重试
+    let newRequest = await request(options, showLoading, auth)
+    return newRequest
+  } else {
+    console.log('结束网络请求')
+    return response
+  }
 }
 
 // 登录
@@ -129,18 +147,29 @@ const formIdSubmit = async (formId) => {
   })
 }
 
+// 申请权限接口
 const getAuthScope = async (scope) => {
   try {
     await wepy.authorize({
       scope: scope
     })
   } catch (e) {
-    let modal = await wepy.showModal({
-      title: '授权被拒绝',
-      content: '您拒绝了我们的权限申请，因此您的操作无法完成。',
-      confirmText: '给予权限',
-      cancelText: '拒绝授权'
-    })
+    let modal
+    if (scope === 'scope.userLocation') {
+      modal = await wepy.showModal({
+        title: '授权被拒绝',
+        content: '您拒绝了我们的获取地理位置权限申请，但要继续您当前的操作，我们必须获得您的地理位置。',
+        confirmText: '给予权限',
+        cancelText: '拒绝授权'
+      })
+    } else {
+      modal = await wepy.showModal({
+        title: '授权被拒绝',
+        content: '您拒绝了我们的权限申请，因此您的操作无法完成。',
+        confirmText: '给予权限',
+        cancelText: '拒绝授权'
+      })
+    }
 
     if (modal.confirm) {
       await wepy.openSetting()
@@ -150,12 +179,14 @@ const getAuthScope = async (scope) => {
   }
 }
 
+// 打开调试模式
 const startDebug = async () => {
   wepy.setEnableDebug({
     enableDebug: true
   })
 }
 
+// 关闭调试模式
 const stopDebug = async () => {
   wepy.setEnableDebug({
     enableDebug: false
